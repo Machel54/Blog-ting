d112dbda7d20fd9ec6792f9e93b940ae357ad860
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-10-27 00:08:29 +0300

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+    
+    app = Flask(__name__)
+    
+    #Creating app configuration
+    app.config.from_object(config_options[config_name])
+    
+    # initializing flask extensions
+    bootstrap.init_app(app)
+    
+    #register blueprints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    return app
