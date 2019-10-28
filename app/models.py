14019d09d5bfcf2eade23c34f8a15d34203a1466
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-10-28 17:05:46 +0300

    creare class blog and initialize it in db

--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
+from datetime import datetime 
+
 
 
 @login_manager.user_loader
@@ -34,3 +36,15 @@ class User(UserMixin,db.Model):
         
     def __repr__(self):
         return f'User {self.username}'
+    
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+    
+    id = db.Column(db.Integer,primary_key = True)
+    blog_title = db.Column(db.String)
+    blog_content = db.Column(db.String(8000))
+    username =  db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    likes = db.Column(db.Integer)
+    dislikes = db.Column(db.Integer)
