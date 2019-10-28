ff16f93f925723b72ccf04616150ff9b74a30d08
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-10-28 16:27:11 +0300

    install uploads and initialize update profile,pic and bio

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 
 bootstrap = Bootstrap()
@@ -12,6 +13,7 @@ mail=Mail()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
     
@@ -19,6 +21,9 @@ def create_app(config_name):
     
     #Creating app configuration
     app.config.from_object(config_options[config_name])
+    
+    # configure UploadSet
+    configure_uploads(app,photos)
     
     # initializing flask extensions
     bootstrap.init_app(app)
